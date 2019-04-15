5926ca68d454e2f3e057df4ac8933d43af94daf3
zorina <zorinamaika.dev@gmail.com>
2019-04-15 17:29:16 +0800

    First commit

---
+++ src/main/java/io/springbootstarter/javabrains/CourseApplication.java
@@ -0,0 +1,12 @@
+package io.springbootstarter.javabrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+}
